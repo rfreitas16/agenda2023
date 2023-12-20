563cca4bfe0bffd39f534888abeef30e65cd2dd9
Rafael <rfreitas16@hotmail.com>
2023-12-20 11:40:32 -0300

    criacao do primeiro model no Django

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+#DOC dos campos https://docs.djangoproject.com/pt-br/4.2/ref/models/fields/#field-choices
+
+#nao esquecer de criar a migracao comando; python manage.py makemigrations
+#nao esquecer de aplicar a migracao para ir pro BD; python manage.py migrate
+
+class Contact(models.Model):
+    #campos obrigatorios / para nao ser obrigadorio acrescentar; blank=True no parametro
+    first_name = models.CharField(max_length=12)
+    last_name = models.CharField(max_length=30)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=30)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
