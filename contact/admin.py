c95c6ef48a7cd4cf2bb9a21ab160b5df125b5a50
Rafael <rfreitas16@hotmail.com>
2023-12-21 10:15:58 -0300

    custumizando a exibicao do list admin

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    ...
+    list_display = 'id','first_name','last_name','phone',
+    ordering = '-id',
+    search_fields = 'first_name','last_name',
+    list_per_page = 1 
+    list_max_show_all = 20
+    # list_editable = 'first_name', 'last_name',
+
+
+    #filtros
+    # list_filter = 'created_date',
--- contact/models.py
+++ contact/models.py
@@ -12,7 +12,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=12)
     last_name = models.CharField(max_length=30)
     phone = models.CharField(max_length=11)
-    email = models.EmailField(max_length=30)
+    email = models.EmailField(max_length=30, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
 
