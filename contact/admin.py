b9f857e133a25d4ed989138426e95df981a67940
Rafael <rfreitas16@hotmail.com>
2023-12-25 11:20:30 -0300

    exibindo ultimos contatos e apliando show mostrar no adm

--- contact/admin.py
+++ contact/admin.py
@@ -4,12 +4,12 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = 'id','first_name','last_name','phone',
+    list_display = 'id','first_name','last_name','phone','show'
     ordering = '-id',
     search_fields = 'first_name','last_name',
     list_per_page = 10 #itens por pagina
     list_max_show_all = 20
-    # list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     #filtros
     # list_filter = 'created_date',
 
