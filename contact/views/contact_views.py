13ee40adafef1f29292afff8bf96d181be4e3c97
Rafael <rfreitas16@hotmail.com>
2023-12-27 09:38:24 -0300

    criando rota de pesquisa SIMPLES

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,6 +1,7 @@
 from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 # Create your views here.
 def index(request):
@@ -9,6 +10,35 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title':  'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    # Q eh um model do django para trocar a consulta AND por OR envolver em parenteses
+    #precisa fazer o importe do model antes.
+
+    contacts = Contact.objects.filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) |
+            Q(phone__icontains=search_value) |
+            Q(email__icontains=search_value) |
+            Q(last_name__icontains=search_value)
+            )\
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title':  'Search - '
     }
 
     return render(
