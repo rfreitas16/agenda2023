0c23928c71588934aa912ad8bdd5de826ddaf3b3
Rafael <rfreitas16@hotmail.com>
2023-12-27 08:40:56 -0300

    criando parciais de template

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -8,6 +8,7 @@ def index(request):
 
     context = {
         'contacts': contacts,
+        'site_title':  'Contatos - '
     }
 
     return render(
@@ -20,8 +21,11 @@ def contact(request, contact_id):
     #metodo get pega um/ pk= primary key
     single_contact = get_object_or_404(Contact.objects, pk=contact_id, show=True)
 
+    site_title = f'{single_contact.first_name}{single_contact.last_name} -'
+
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render(
